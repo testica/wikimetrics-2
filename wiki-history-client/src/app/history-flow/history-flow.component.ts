25c90ab434a7007d217229ff2fbde49c345625f1
Leonardo Testa <leitotesta@gmail.com>
2018-02-16 10:47:51 -0400

    agregar filtro por fecha (#93)

--- wiki-history-client/src/app/history-flow/history-flow.component.ts
+++ wiki-history-client/src/app/history-flow/history-flow.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { WikimetricsService, WikimetricsRevision } from '../wikimetrics.service';
-import { ArticleService, Article } from '../article.service';
+import { Article } from '../article.service';
 import { NavbarService } from '../navbar/navbar.service';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+
+import { DateTime } from 'luxon';
 
 
 @Component({
@@ -16,33 +18,65 @@ import { NavbarService } from '../navbar/navbar.service';
 })
 
 export class HistoryFlowComponent implements OnInit {
-  revisions$: Observable<WikimetricsRevision[]>;
-  article$: Observable<Article>;
+  revisions$ = new ReplaySubject<WikimetricsRevision[]>(1);
+  article: Article;
 
   visualizationLoaded$ = new BehaviorSubject<boolean>(false);
 
+  destroy = false;
+  fromDate;
+  toDate;
+
   constructor(
     private wikimetricsSvc: WikimetricsService,
-    private articleSvc: ArticleService,
     private route: ActivatedRoute,
     private navbarSvc: NavbarService,
     private cdr: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
-    // listen title from url
-    this.article$ = this.route.paramMap.switchMap(params => {
-      return this.articleSvc.get({ title: params.get('title'), locale: params.get('locale') } as Article);
-    }).shareReplay();
 
-    this.revisions$ = this.article$.switchMap(art =>
-      this.wikimetricsSvc.count({ locale: art.locale, title: art.title }).switchMap(count =>
-        this.wikimetricsSvc.revisions({ locale: art.locale, title: art.title, page_size: count, sort: 'asc' })
-      )
-    ).shareReplay();
+    this.article = {
+      locale: this.route.snapshot.params.locale,
+      title: this.route.snapshot.params.title
+    };
+
+    this.wikimetricsSvc.revisions({
+      locale: this.article.locale,
+      title: this.article.title,
+      page_size: 200, sort: 'asc'
+    })
+    .subscribe(revs => {
+      this.fromDate = DateTime.fromJSDate(new Date(revs[0].timestamp)).toISODate();
+      this.toDate = DateTime.fromJSDate(new Date(revs[revs.length - 1].timestamp)).toISODate();
+      this.revisions$.next(revs);
+    });
 
     // setting navbar
     this.navbarSvc.config$.next({ title: 'Wiki History Flow', showUser: true });
+  }
+
+  apply() {
+    this.loading(false);
+    this.destroy = true;
+    this.wikimetricsSvc.customQuery(this.article, [
+      {
+        '$match': {
+          'timestamp': {
+            '$gt' : DateTime.fromJSDate(new Date(this.fromDate)).toISODate(),
+            '$lt' : DateTime.fromJSDate(new Date(this.toDate)).toISODate()
+          }
+        }
+      },
+      {
+        '$project': { '*': 1, 'timestamp': 1, 'user': 1, 'size': 1 }
+      }
+    ])
+    .subscribe((revs: any[]) => {
+      revs.map(rev => rev.timestamp = DateTime.fromJSDate(new Date(rev.timestamp.$date)).toISODate());
+      this.revisions$.next(revs);
+      this.destroy = false;
+    });
   }
 
   loading(loaded: boolean) {
