e459f55b7a0480777baf02d12e4c5fa100200e3f
Leonardo Testa <leitotesta@gmail.com>
2018-05-05 03:49:03 -0400

    corregir api url

--- wiki-history-client/src/environments/environment.prod.ts
+++ wiki-history-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:4242',
+  API_URL: 'http://190.169.79.33:4242',
   WIKIPEDIA_API: 'https://{locale}.wikipedia.org/w/api.php?action=opensearch&format=json&callback=JSONP_CALLBACK',
   WIKIMETRICS_API: 'http://190.169.79.33/api/v1',
   LOCALES: ['en', 'es']
