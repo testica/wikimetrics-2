1b7db23f231ead7c2d020b43fa9fc9ffa3434dbf
testica <leitotesta@gmail.com>
2018-05-26 01:37:21 -0400

    mejorar navegación de la página

--- wiki-history-client/src/app/navbar/navbar.component.ts
+++ wiki-history-client/src/app/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+
 import { Observable } from 'rxjs/Observable';
+
 import { NavbarService, NavbarConfig } from './navbar.service';
 import { AuthService } from '../auth.service';
 
@@ -16,13 +19,18 @@ export class NavbarComponent {
 
   constructor(
     private navbarSvc: NavbarService,
-    private authSvc: AuthService
+    private authSvc: AuthService,
+    private localion: Location
   ) {
     this.config$ = this.navbarSvc.config$;
   }
 
   clickButton() {
     this.navbarSvc.click$.next();
+  }
+
+  goBack() {
+    this.localion.back();
   }
 
   signOut() {
